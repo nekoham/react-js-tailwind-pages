e6c6f0f3283fc02da5b87d5e1f4732ff9a047529
Woramet <68908184+nekoham@users.noreply.github.com>
2023-04-23 12:46:21 +0700

    Add 3 Navbar style

--- react-tailwind-app/src/App.js
+++ react-tailwind-app/src/App.js
@@ -1,11 +1,14 @@
 import Login from "./components/Form";
 import OrderSpace from "./components/OrderSpace";
-import NavBar from "./components/NavBar.js";
+import { GuestNavbar, UserNavbar, AuthNavbar } from "./components/NavBar.js";
 
 function App() {
   return (
-    <div className="app">
-      <NavBar/>
+      <div className="app">
+      //Showing 3 of them individually
+      <AuthNavbar />
+      <UserNavbar />
+      <GuestNavbar/>
       <Login/>
       <OrderSpace/>
     </div>
